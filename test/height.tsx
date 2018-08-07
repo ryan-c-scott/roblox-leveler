a6c3c18a9eddf15f1ab96351998831e7139228d8
Ryan Scott <rscott@roblox.com>
2018-08-07 14:06:15 -0700

    Updated some test data

---
+++ test/height.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fortnite_athena_height_l16_preview" tilewidth="512" tileheight="512" tilecount="1" columns="1">
+ <image source="height.png" width="512" height="512"/>
+</tileset>
