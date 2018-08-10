4fb78393c80c00dd6d1682e4d7ec34f160903346
Ryan Scott <rscott@roblox.com>
2018-08-10 15:31:45 -0700

    Updated test data to properly map to be a 4k area with a fragment size of 150^2

--- test/height.tsx
+++ test/height.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="fortnite_athena_height_l16_preview" tilewidth="512" tileheight="512" tilecount="1" columns="1">
- <image source="height.png" width="512" height="512"/>
+<tileset name="fortnite_athena_height_l16_preview" tilewidth="3000" tileheight="3000" tilecount="1" columns="1">
+ <grid orientation="orthogonal" width="3000" height="3000"/>
+ <image source="height.png" width="3000" height="3000"/>
 </tileset>
